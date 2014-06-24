d1545fc0f96aac1956c121ea0d769b2b537bccdb
Brian Iversen <biversen21@gmail.com>
2014-06-24 14:00:44 -0700

    setup array file

---
+++ underscore2.js
@@ -0,0 +1,39 @@
+function first(array, n) {
+	
+}
+
+function initial(array, n) {
+	
+}
+
+function last(array, n) {
+	
+}
+
+function rest(array, index) {
+	
+}
+
+function compact(array) {
+	
+}
+
+function flatten(array, shallow) {
+	
+}
+
+function without(array, values) {
+	
+}
+
+function partition(array, predicate) {
+	
+}
+
+function union(arrays) {
+	
+}
+
+function intersection(arrays) {
+	
+}
